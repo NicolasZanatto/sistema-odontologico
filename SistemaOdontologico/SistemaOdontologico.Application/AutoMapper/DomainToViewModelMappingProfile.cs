5fd6ba6fec4e46c5e85b58f35a35d23a0a2f2740
NicolasZanatto <34211411+NicolasZanatto@users.noreply.github.com>
2018-11-13 17:57:01 -0200

    Inicio ViewModel e AutoMapper

---
+++ SistemaOdontologico/SistemaOdontologico.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using SistemaOdontologico.Application.ViewModels;
+using SistemaOdontologico.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaOdontologico.Application.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        public DomainToViewModelMappingProfile()
+        {
+            
+        }
+    }
+}
